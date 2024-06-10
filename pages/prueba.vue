23063271a905378cfe7d9ec8c1a006fb5a751635
Jose Gutierrez <jo0303se@hotmail.com>
2024-06-09 22:27:53 -0500

    commit

--- pages/prueba.vue
+++ pages/prueba.vue
@@ -1,21 +1,36 @@
 <template>
   <div ref="mainContainer">
+    <input @click="click" v-model="input" />
     <ContenidoCardContainer>
       <ContenidoCard
-        v-for="pokemon in data.results"
+        v-for="pokemon in results"
         :key="pokemon.name"
         :url="pokemon.url"
       />
+      <!-- <div v-for="pokemon in results">
+        {{ pokemon.name }}
+      </div> -->
     </ContenidoCardContainer>
   </div>
 </template>
 
 <script setup>
+const urlBase = "https://pokeapi.co/api/v2/pokemon?offset=0&limit=";
 const limit = ref(40);
-const url = computed(
-  () => `https://pokeapi.co/api/v2/pokemon?offset=0&limit=${limit.value}`
-);
-const { data } = await useFetch(url, { cache: "default" });
+const input = ref("");
+const url = computed(() => urlBase + limit.value);
+const { data, refresh } = await useFetch(url);
+const data2 = await $fetch(urlBase + "1302");
+
+const click = () => refresh();
+
+const results = computed(() => {
+  if(input.value === "") {
+    return data.value.results;
+  }
+  if(limit.value !== 40) limit.value = 40;
+  return data2.results.filter(v => v.name.includes(input.value));
+});
 
 const mainContainer = ref();
 onMounted(() => {
